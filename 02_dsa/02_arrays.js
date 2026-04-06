471e01ece7eb2cd9fc646562562fbc588e90c350
Ankit-Vashisth-Sharma <vashisth.codes@gmail.com>
2026-04-06 19:09:50 +0530

    1.4

--- 02_dsa/02_arrays.js
+++ 02_dsa/02_arrays.js
@@ -22,3 +22,16 @@ console.log(r1[3][3][1]); // 8
 console.log(r1.flat(2)); // [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 console.log(Array.isArray(r1)); 
+
+console.log(Array.isArray("Anything"));
+
+console.log(Array.from("Anything")); // ["A", "n", "y", "t", "h", "i", "n", "g"]
+
+let b1 = 100;
+let b2 = 200;
+let b3 = 300;
+
+let arr = [b1, b2, b3];
+console.log(arr); // [100, 200, 300]
+
+console.log(Array.of(b1, b2, b3)); // [100, 200, 300]
