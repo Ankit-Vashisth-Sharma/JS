234b21da2668a7087583da81680de42fa4a2285a
Ankit-Vashisth-Sharma <vashisth.codes@gmail.com>
2026-04-07 16:28:12 +0530

    1.7

---
+++ 02_dsa/05_objects3.js
@@ -0,0 +1,25 @@
+const course = {
+    courseName: "js in hindi",
+    price: "999",
+    courseIns: "abc"
+}
+
+console.log(course);
+console.log(course.courseIns)
+
+const {courseIns: cs1} = course
+
+console.log(cs1);
+
+// json format js object notation
+// {
+//     "name": "ankit",
+//     "courseName": "XYZ",
+//     "price": "free"
+// }
+
+[
+    {},
+    {},
+    {}
+]
