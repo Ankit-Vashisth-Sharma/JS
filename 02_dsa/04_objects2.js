a56a075e66ec7c8fb397935805a102185aca5326
Ankit-Vashisth-Sharma <vashisth.codes@gmail.com>
2026-04-07 16:08:04 +0530

    1.6

--- 02_dsa/04_objects2.js
+++ 02_dsa/04_objects2.js
@@ -17,17 +17,28 @@ const regularUser = {
         }
     }
 }
-console.log(regularUser.email)
-console.log(regularUser.fullName.userfullName.firstName)
-console.log(regularUser.fullName.userfullName.lastName)
-console.log(regularUser.fullName)
+// console.log(regularUser.email)
+// console.log(regularUser.fullName.userfullName.firstName)
+// console.log(regularUser.fullName.userfullName.lastName)
+// console.log(regularUser.fullName)
 
 const obj1 = {1: "a", 2: "b"};
 const obj2 = {3: "c", 4: "d"};
+const obj5 = {5: "e", 6: "f"};
 
 const obj3 = {obj1,obj2};
 const obj4 = Object.assign(obj1,obj2)
+const obj6 = Object.assign({},obj1,obj2,obj5)
 
-console.log(obj3)
-console.log(obj4)
+// console.log(obj3)
+// console.log(obj4)
+// console.log(obj6)
 
+const spdobj = {...obj1, ...obj2 , ...obj5, ...obj3, ...obj4, ...obj6}
+console.log(spdobj);
+
+console.log(obj1[2])
+console.log(Object.keys(obj1))
+console.log(Object.values(obj1))
+console.log(Object.entries(tinderUser))
+console.log(tinderUser.hasOwnProperty('isLoggedIn'))
